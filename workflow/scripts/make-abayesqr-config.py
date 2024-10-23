7a673f1b758b042411d31a8636773a1365129fdd
David Pattinson <davipatti@protonmail.com>
2024-10-23 15:42:26 -0400

    Cleaning up make-abayesqr-config.py.

--- workflow/scripts/make-abayesqr-config.py
+++ workflow/scripts/make-abayesqr-config.py
@@ -4,6 +4,19 @@ from pathlib import Path
 import argparse
 from Bio.SeqIO import parse
 
+TEMPLATE = """filename of reference sequence (FASTA) : {fasta}
+filname of the aligned reads (sam format) : {sam}
+paired-end (1 = true, 0 = false) : 1
+SNV_thres : 0.05
+reconstruction_start : 1
+reconstruction_stop: {length}
+min_mapping_qual : 60
+min_read_length : 100
+max_insert_length : 10
+characteristic zone name : abayesqr
+seq_err (assumed sequencing error rate(%)) : 0.1
+MEC improvement threshold : 0.0395"""
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser("make-abayesqr-config.py")
@@ -21,20 +34,9 @@ if __name__ == "__main__":
     with open(args.fasta, "r") as fobj:
         record = next(parse(fobj, format="fasta"))
 
-    filename = Path(args.fasta).name
-
-    config = f"""filename of reference sequence (FASTA) : {filename}
-    filname of the aligned reads (sam format) : {args.sam}
-    paired-end (1 = true, 0 = false) : 1
-    SNV_thres : 0.05
-    reconstruction_start : 1
-    reconstruction_stop: {len(record.seq)}
-    min_mapping_qual : 60
-    min_read_length : 100
-    max_insert_length : 10
-    characteristic zone name : abayesqr
-    seq_err (assumed sequencing error rate(%)) : 0.1
-    MEC improvement threshold : 0.0395
-    """
-
-    print(config)
+    print(
+        TEMPLATE.format(
+            fasta=Path(args.fasta).name, sam=args.sam, length=len(record.seq)
+        ),
+        end="",
+    )
